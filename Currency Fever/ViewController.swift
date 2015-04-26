4c8400fb157ed5bbd8667de88b28575c1594e705
ken <ken@innomonster.com>
2015-04-27 01:10:04 +0800

    Initial Commit

---
+++ Currency Fever/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency Fever
+//
+//  Created by Ken on 27/4/15.
+//  Copyright (c) 2015 North Borneo Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Currency FeverTests/Currency_FeverTests.swift
@@ -0,0 +1,36 @@
+//
+//  Currency_FeverTests.swift
+//  Currency FeverTests
+//
+//  Created by Ken on 27/4/15.
+//  Copyright (c) 2015 North Borneo Interactive. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Currency_FeverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
